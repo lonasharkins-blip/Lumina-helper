5c78bfb43f0cd985c86351a959ca3122cbfdbe0e
lonasharkins-blip <lonasharkins@gmail.com>
2026-08-22 17:55:15 -0300

    fix: complete initial build configuration

--- app/src/main/java/com/lonasharkins/luminahelper/MainActivity.kt
+++ app/src/main/java/com/lonasharkins/luminahelper/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -258,4 +259,3 @@ private fun InstrumentKeys(profile: InstrumentProfile) {
         }
     }
 }
-
